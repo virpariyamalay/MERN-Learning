f9eba8b34234cd97e7653f94b784bbaf8efffc16
virpariyamalay <mpcvirpariya@gmail.com>
2025-02-12 10:07:02 +0530

    node done

---
+++ backend/node/math.js
@@ -0,0 +1,4 @@
+export let sum = (a, b) => a + b;
+export let mul = (a, b) => a * b;
+
+
---
+++ backend/node/script.js
@@ -0,0 +1,40 @@
+// let n = 5;
+// for (let i = 0; i < n; i++) {
+//     console.log("hello" + i)
+// }
+
+// console.log("bye ");
+
+// let name1 = process.argv;
+
+// for (let i = 2; i < name1.length; i++) {
+//     console.log("hello " + name1[i] + " how are you!");
+// }
+// console.log(process.argv);
+
+
+// let somevalue = require('./math');
+
+// console.log(somevalue);
+
+// const figlet = require("figlet");
+
+// figlet("GLOBAL MALAY!!", function (err, data) {
+//     if (err) {
+//         console.log("Something went wrong...");
+//         console.dir(err);
+//         return;
+//     }
+//     console.log(data);
+// });
+
+// import { sum, mul } from "./math.js"
+// console.log(sum(1, 2));
+// console.log(mul(5, 2.0));
+
+
+import { generate, } from "random-words";
+
+console.log(generate());
+
+
